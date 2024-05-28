83bc69a0e1783b68338e202d1705ab82c341856f
Usuario <Usuario@DESARROLLO10.local>
2024-05-28 10:43:30 +0200

    Clases principales completas
    Se ha terminado de implementar la clase Mano

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -36,6 +36,16 @@ public class Mano extends Mazo {
 		return "Mano [" + cartas + "]Valor de la mano:"+valorMano;
 	}
 	
-	
+	public void pedirCarta(Mazo baraja) {
+		//Recoge la carta que se encuentra en el principio del Mazo
+		Carta c = baraja.solicitarCarta();
+		this.cartas.add(c);
+		if (valorMano()>21) {
+			throw new Masde21Exception():
+		}
+		if (valorMano()==21) {
+			throw new HayBlackjackException();
+		}
+	}
 	
 }
