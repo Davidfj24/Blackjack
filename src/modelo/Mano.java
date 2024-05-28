8b469e59532c2119e8706dd13005f469d0215251
Usuario <Usuario@DESARROLLO10.local>
2024-05-28 09:17:42 +0200

    Clase mano modificada
    modificado el constructor y añadido el metodo valorMano que devuelve la
    puntuacion de la mano. Falta implementar el caso del AS que pueda valer
    1 u 11
    Clse Mano modificada
    Modificado el metodo valorMano oara que los ases puedan valer tanto 1
    como 11, añadido el metodo finDeJuego y toString

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -1,9 +1,41 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Mano extends Mazo {
 
 	public Mano() {
+		// CREAMOS UNA LIST DE CARTAS VACÍA.
+		this.cartas = new ArrayList<Carta>();
+	}
+	
+	public int valorMano() throws Masde21Exception {
+		int valor=0;
+		int numAses=0;
+		for(Carta c : this.cartas) {
+			if (c.getNumero()==1) {
+				numAses++;
+			}
+			valor = valor + c.getValor();
+		
+		if (valor>21) {
+			throw new Masde21Exception();
+		}
+		return valor;
+	}
+	
+	public boolean finDeJuego() {
+		if (valorMano()>=21) {
+			return true;
+		}
+		return false;
 	}
 
+	@Override
+	public String toString() {
+		return "Mano [" + cartas + "]Valor de la mano:"+valorMano;
+	}
+	
+	
 	
 }
